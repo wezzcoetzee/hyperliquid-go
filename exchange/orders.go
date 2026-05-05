109e2d5fb4c9e8174914c3e1e85da3f37928a319
Wesley Coetzee <wesley-coetzee@idexx.com>
2026-05-05 13:32:03 +1200

    feat(exchange): add Cancel, Modify, BatchModify, ScheduleCancel, UpdateLeverage, UpdateIsolatedMargin, TWAP

--- exchange/orders.go
+++ exchange/orders.go
@@ -114,3 +114,207 @@ func (c *Client) Order(ctx context.Context, req OrderRequest) (*OrderResponse, e
 	}
 	return &OrderResponse{Statuses: inner.Data.Statuses}, nil
 }
+
+// CancelParams identifies one perp order to cancel by oid.
+type CancelParams struct {
+	Asset uint32
+	Oid   uint64
+}
+
+func buildCancelAction(cancels []CancelParams) *msgpack.OrderedMap {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "cancel")
+	arr := make([]any, len(cancels))
+	for i, c := range cancels {
+		om := msgpack.NewOrderedMap()
+		om.Set("a", uint64(c.Asset))
+		om.Set("o", c.Oid)
+		arr[i] = om
+	}
+	a.Set("cancels", arr)
+	return a
+}
+
+// CancelResponse is the inner response shape for cancel/cancelByCloid.
+type CancelResponse struct {
+	Statuses []string `json:"statuses"`
+}
+
+// Cancel cancels one or more orders by oid.
+func (c *Client) Cancel(ctx context.Context, cancels []CancelParams) (*CancelResponse, error) {
+	action := buildCancelAction(cancels)
+	var inner struct {
+		Type string         `json:"type"`
+		Data CancelResponse `json:"data"`
+	}
+	if err := c.submitL1(ctx, action, &inner); err != nil {
+		return nil, err
+	}
+	return &inner.Data, nil
+}
+
+// CancelByCloidParams identifies one order to cancel by client-supplied cloid.
+type CancelByCloidParams struct {
+	Asset uint32
+	Cloid string
+}
+
+func buildCancelByCloidAction(cancels []CancelByCloidParams) *msgpack.OrderedMap {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "cancelByCloid")
+	arr := make([]any, len(cancels))
+	for i, c := range cancels {
+		om := msgpack.NewOrderedMap()
+		om.Set("asset", uint64(c.Asset))
+		om.Set("cloid", c.Cloid)
+		arr[i] = om
+	}
+	a.Set("cancels", arr)
+	return a
+}
+
+// CancelByCloid cancels one or more orders by client-supplied cloid.
+func (c *Client) CancelByCloid(ctx context.Context, cancels []CancelByCloidParams) (*CancelResponse, error) {
+	action := buildCancelByCloidAction(cancels)
+	var inner struct {
+		Type string         `json:"type"`
+		Data CancelResponse `json:"data"`
+	}
+	if err := c.submitL1(ctx, action, &inner); err != nil {
+		return nil, err
+	}
+	return &inner.Data, nil
+}
+
+// ModifyParams modifies one resting order to new price/size/order-type.
+type ModifyParams struct {
+	Oid      uint64
+	NewOrder OrderParams
+}
+
+func buildOneOrder(o OrderParams) *msgpack.OrderedMap {
+	om := msgpack.NewOrderedMap()
+	om.Set("a", uint64(o.Asset))
+	om.Set("b", o.IsBuy)
+	om.Set("p", o.LimitPx)
+	om.Set("s", o.Sz)
+	om.Set("r", o.ReduceOnly)
+	t := msgpack.NewOrderedMap()
+	switch {
+	case o.OrderType.Limit != nil:
+		lim := msgpack.NewOrderedMap()
+		lim.Set("tif", string(o.OrderType.Limit.Tif))
+		t.Set("limit", lim)
+	case o.OrderType.Trigger != nil:
+		trg := msgpack.NewOrderedMap()
+		trg.Set("isMarket", o.OrderType.Trigger.IsMarket)
+		trg.Set("triggerPx", o.OrderType.Trigger.TriggerPx)
+		trg.Set("tpsl", o.OrderType.Trigger.Tpsl)
+		t.Set("trigger", trg)
+	}
+	om.Set("t", t)
+	if o.Cloid != "" {
+		om.Set("c", o.Cloid)
+	}
+	return om
+}
+
+func buildModifyAction(p ModifyParams) *msgpack.OrderedMap {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "modify")
+	a.Set("oid", p.Oid)
+	a.Set("order", buildOneOrder(p.NewOrder))
+	return a
+}
+
+// Modify modifies one resting order.
+func (c *Client) Modify(ctx context.Context, p ModifyParams) error {
+	return c.submitL1(ctx, buildModifyAction(p), nil)
+}
+
+func buildBatchModifyAction(modifies []ModifyParams) *msgpack.OrderedMap {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "batchModify")
+	arr := make([]any, len(modifies))
+	for i, m := range modifies {
+		om := msgpack.NewOrderedMap()
+		om.Set("oid", m.Oid)
+		om.Set("order", buildOneOrder(m.NewOrder))
+		arr[i] = om
+	}
+	a.Set("modifies", arr)
+	return a
+}
+
+// BatchModify modifies several resting orders atomically.
+func (c *Client) BatchModify(ctx context.Context, modifies []ModifyParams) error {
+	return c.submitL1(ctx, buildBatchModifyAction(modifies), nil)
+}
+
+// ScheduleCancel arms a dead-man's-switch. Pass deadlineMs == 0 to disarm.
+func (c *Client) ScheduleCancel(ctx context.Context, deadlineMs int64) error {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "scheduleCancel")
+	if deadlineMs > 0 {
+		a.Set("time", uint64(deadlineMs))
+	}
+	return c.submitL1(ctx, a, nil)
+}
+
+// UpdateLeverage changes the leverage setting on one perp asset.
+func (c *Client) UpdateLeverage(ctx context.Context, asset uint32, isCross bool, leverage int) error {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "updateLeverage")
+	a.Set("asset", uint64(asset))
+	a.Set("isCross", isCross)
+	a.Set("leverage", uint64(leverage))
+	return c.submitL1(ctx, a, nil)
+}
+
+// UpdateIsolatedMargin adds (positive) or removes (negative) margin on an isolated perp position.
+func (c *Client) UpdateIsolatedMargin(ctx context.Context, asset uint32, isBuy bool, ntli int64) error {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "updateIsolatedMargin")
+	a.Set("asset", uint64(asset))
+	a.Set("isBuy", isBuy)
+	a.Set("ntli", ntli)
+	return c.submitL1(ctx, a, nil)
+}
+
+// TwapParams describes a TWAP order request.
+type TwapParams struct {
+	Asset      uint32
+	IsBuy      bool
+	Sz         string
+	ReduceOnly bool
+	Minutes    int
+	Randomize  bool
+}
+
+func buildTwapOrderAction(p TwapParams) *msgpack.OrderedMap {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "twapOrder")
+	t := msgpack.NewOrderedMap()
+	t.Set("a", uint64(p.Asset))
+	t.Set("b", p.IsBuy)
+	t.Set("s", p.Sz)
+	t.Set("r", p.ReduceOnly)
+	t.Set("m", uint64(p.Minutes))
+	t.Set("t", p.Randomize)
+	a.Set("twap", t)
+	return a
+}
+
+// TwapOrder submits a TWAP order.
+func (c *Client) TwapOrder(ctx context.Context, p TwapParams) error {
+	return c.submitL1(ctx, buildTwapOrderAction(p), nil)
+}
+
+// TwapCancel cancels a running TWAP by id.
+func (c *Client) TwapCancel(ctx context.Context, asset uint32, twapID uint64) error {
+	a := msgpack.NewOrderedMap()
+	a.Set("type", "twapCancel")
+	a.Set("a", uint64(asset))
+	a.Set("t", twapID)
+	return c.submitL1(ctx, a, nil)
+}
--- exchange/orders_test.go
+++ exchange/orders_test.go
@@ -117,3 +117,216 @@ func TestClient_Order_NoSigner(t *testing.T) {
 		t.Fatalf("expected ErrNoSigner, got %v", err)
 	}
 }
+
+func TestClient_Cancel(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "cancel" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		cancels := action["cancels"].([]any)
+		if len(cancels) != 1 {
+			t.Fatalf("cancels: %v", cancels)
+		}
+		first := cancels[0].(map[string]any)
+		if first["a"].(float64) != 0 || first["o"].(float64) != 12345 {
+			t.Fatalf("first: %v", first)
+		}
+		return map[string]any{
+			"status": "ok",
+			"response": map[string]any{
+				"type": "cancel",
+				"data": map[string]any{"statuses": []any{"success"}},
+			},
+		}
+	})
+	defer srv.Close()
+
+	resp, err := c.Cancel(context.Background(), []CancelParams{{Asset: 0, Oid: 12345}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.Statuses) != 1 || resp.Statuses[0] != "success" {
+		t.Fatalf("statuses: %v", resp.Statuses)
+	}
+}
+
+func TestClient_CancelByCloid(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "cancelByCloid" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		cancels := action["cancels"].([]any)
+		first := cancels[0].(map[string]any)
+		if first["asset"].(float64) != 0 || first["cloid"] != "0x0123456789abcdef0123456789abcdef" {
+			t.Fatalf("first: %v", first)
+		}
+		return map[string]any{
+			"status": "ok",
+			"response": map[string]any{
+				"type": "cancel",
+				"data": map[string]any{"statuses": []any{"success"}},
+			},
+		}
+	})
+	defer srv.Close()
+
+	_, err := c.CancelByCloid(context.Background(), []CancelByCloidParams{{Asset: 0, Cloid: "0x0123456789abcdef0123456789abcdef"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_Modify(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "modify" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		if action["oid"].(float64) != 555 {
+			t.Fatalf("oid: %v", action["oid"])
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.Modify(context.Background(), ModifyParams{
+		Oid: 555,
+		NewOrder: OrderParams{
+			Asset: 0, IsBuy: true, LimitPx: "30100", Sz: "0.1",
+			OrderType: OrderType{Limit: &LimitOrder{Tif: TifGtc}},
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_BatchModify(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "batchModify" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		mods := action["modifies"].([]any)
+		if len(mods) != 2 {
+			t.Fatalf("len(mods)=%d", len(mods))
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.BatchModify(context.Background(), []ModifyParams{
+		{Oid: 1, NewOrder: OrderParams{Asset: 0, IsBuy: true, LimitPx: "1", Sz: "0.1", OrderType: OrderType{Limit: &LimitOrder{Tif: TifGtc}}}},
+		{Oid: 2, NewOrder: OrderParams{Asset: 0, IsBuy: false, LimitPx: "2", Sz: "0.1", OrderType: OrderType{Limit: &LimitOrder{Tif: TifGtc}}}},
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_ScheduleCancel(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "scheduleCancel" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		if action["time"].(float64) != 1700000000000 {
+			t.Fatalf("time: %v", action["time"])
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.ScheduleCancel(context.Background(), 1700000000000); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_ScheduleCancel_Disarm(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if _, present := action["time"]; present {
+			t.Fatalf("time should be omitted for disarm")
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.ScheduleCancel(context.Background(), 0); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_UpdateLeverage(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "updateLeverage" || action["asset"].(float64) != 0 || action["isCross"] != true || action["leverage"].(float64) != 10 {
+			t.Fatalf("action: %v", action)
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.UpdateLeverage(context.Background(), 0, true, 10); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_UpdateIsolatedMargin(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "updateIsolatedMargin" || action["ntli"].(float64) != -100 {
+			t.Fatalf("action: %v", action)
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.UpdateIsolatedMargin(context.Background(), 0, true, -100); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_TwapOrder(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "twapOrder" {
+			t.Fatalf("type: %v", action["type"])
+		}
+		twap := action["twap"].(map[string]any)
+		if twap["m"].(float64) != 30 {
+			t.Fatalf("twap: %v", twap)
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.TwapOrder(context.Background(), TwapParams{Asset: 0, IsBuy: true, Sz: "0.1", Minutes: 30, Randomize: true}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_TwapCancel(t *testing.T) {
+	pk, _ := privkey.New("0x0000000000000000000000000000000000000000000000000000000000000001")
+	c, srv := newExchangeTestClient(t, pk, SourceMainnet, 42161, func(req map[string]any) any {
+		action := req["action"].(map[string]any)
+		if action["type"] != "twapCancel" || action["t"].(float64) != 7 {
+			t.Fatalf("action: %v", action)
+		}
+		return map[string]any{"status": "ok", "response": map[string]any{}}
+	})
+	defer srv.Close()
+
+	if err := c.TwapCancel(context.Background(), 0, 7); err != nil {
+		t.Fatal(err)
+	}
+}
